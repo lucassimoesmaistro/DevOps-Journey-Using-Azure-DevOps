46e3002fc2713ca98afa362caddbe60a2d5f440d
Lucas <lucassimoes_@hotmail.com>
2022-09-05 09:07:56 -0300

    region brazilsouth

--- labs/1-Initial-Setup/scripts/create-terraform-storage.sh
+++ labs/1-Initial-Setup/scripts/create-terraform-storage.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 RESOURCE_GROUP_NAME="devops-journey-rg"
-STORAGE_ACCOUNT_NAME="devopsjourneyazuredevops"
+STORAGE_ACCOUNT_NAME="devopsjourneylucas"
 
 # Create Resource Group
-az group create -l uksouth -n $RESOURCE_GROUP_NAME
+az group create -l brazilsouth -n $RESOURCE_GROUP_NAME
 
 # Create Storage Account
-az storage account create -n $STORAGE_ACCOUNT_NAME -g $RESOURCE_GROUP_NAME -l uksouth --sku Standard_LRS
+az storage account create -n $STORAGE_ACCOUNT_NAME -g $RESOURCE_GROUP_NAME -l brazilsouth --sku Standard_LRS
 
 # Create Storage Account blob
 az storage container create  --name tfstate --account-name $STORAGE_ACCOUNT_NAME
